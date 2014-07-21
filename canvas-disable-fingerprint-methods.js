e85d8eddaaf5177baac99f6d255998276c0ea6a1
Sean McIntyre <s.mcintyre@xverba.ca>
2014-07-21 18:56:22 -0400

    Display a useful message to the user when a site attempts to read image data from a canvas, so they know what's going on and how to restore functionality if they want to.

--- canvas-disable-fingerprint-methods.js
+++ canvas-disable-fingerprint-methods.js
@@ -1,16 +1,29 @@
+//alert functionality
+var _canvasDisableFingerprintAlertDisplayed = false;
+var alertUser = function() {
+  if (!_canvasDisableFingerprintAlertDisplayed) {
+    alert('I am trying to read image data from a canvas, a method which can be used to track your browser.\n\nIf you want to allow this, disable the \'Canvas Fingerprint Eliminator\' extension and reload the page.');
+  }
+  _canvasDisableFingerprintAlertDisplayed = true;
+}
+
+//disable stuff
 var originalGetContextFunction = HTMLCanvasElement.prototype.getContext;
 
 HTMLCanvasElement.prototype.getContext = function() {
-  console.log('getImageData() has been removed from contexts retrieved from HTMLCanvasElements to guard against canvas fingerprinting. To restore functionality, disable the \'Canvas Fingerprint Eliminator\' extension.');
+  console.log('getImageData() has been removed from contexts retrieved from HTMLCanvasElements to guard against canvas fingerprinting. To restore functionality, disable the \'Canvas Fingerprint Eliminator\' extension and reload the page.');
+  alertUser();
   var ctx = originalGetContextFunction.apply(this, arguments);
   delete ctx.getImageData;
   return ctx;
 };
 
 HTMLCanvasElement.prototype.toDataURL = function() {
-  console.log('toDataURL() has been removed from HTMLCanvasElements to guard against canvas fingerprinting. To restore functionality, disable the \'Canvas Fingerprint Eliminator\' extension.');
+  console.log('toDataURL() has been removed from HTMLCanvasElements to guard against canvas fingerprinting. To restore functionality, disable the \'Canvas Fingerprint Eliminator\' extension and reload the page.');
+  alertUser();  
 };
 
 HTMLCanvasElement.prototype.toBlob = function() {
-  console.log('toBlob() has been removed from HTMLCanvasElements to guard against canvas fingerprinting. To restore functionality, disable the \'Canvas Fingerprint Eliminator\' extension.');
+  console.log('toBlob() has been removed from HTMLCanvasElements to guard against canvas fingerprinting. To restore functionality, disable the \'Canvas Fingerprint Eliminator\' extension and reload the page.');
+  alertUser();  
 };
--- canvas-do-not-track.js
+++ canvas-do-not-track.js
@@ -1,7 +1,8 @@
+//with help from https://stackoverflow.com/questions/14165629/add-javascript-file-to-all-sites-i-browse
 var script = document.createElement('script'); // Create a Script Tag
 script.src = chrome.extension.getURL('canvas-disable-fingerprint-methods.js'); //Fetch the content script
 script.onload = function () {
     this.parentNode.removeChild(this); //Remove script after script executed
 };
 (document.head || document.documentElement).appendChild(script); //ADD script tag 
-                                                       //to head or Html element
+                                                                 //to head or Html element
